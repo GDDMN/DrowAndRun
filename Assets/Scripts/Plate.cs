c39ef932077385db409261aa4aa532356c11e44c
GDDMN <apawkin@gmail.com>
2024-05-18 14:40:15 +0300

    fix drawing

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -9,6 +9,8 @@ public class Plate : MonoBehaviour
 
   [SerializeField] private List<GameObject> _allPlayers;
 
+  [SerializeField] private int _unitsCount = 16;
+
   private void Start()
   {
     _drawPanel.OnPlayersSet += CreatePlayersOnPlate;
@@ -18,13 +20,47 @@ public class Plate : MonoBehaviour
   {
     DestroyAllPlayer();
 
-    foreach (var pos in positions)
+    float lenght = 0f;
+
+    for(int i=1;i<positions.Count; i++)
     {
-      Vector3 localPos = new Vector3(pos.x, 0f, pos.y);
+      lenght += Vector2.Distance(positions[i], positions[i - 1]);   
+    }
+
+    float stepLenght = lenght / _unitsCount;
+    float currentPathLenght = 0f;
+
+    for(int i=0;i<_unitsCount;i++)
+    {
+      Vector3 localPos = GetPoint(positions, currentPathLenght);
+      currentPathLenght += stepLenght;
       GameObject player = Instantiate(_prefab, transform);
       player.transform.localPosition = localPos;
       _allPlayers.Add(player);
     }
+  }
+
+  private Vector3 GetPoint(List<Vector2> positions, float targetDistance)
+  {
+    for (int i = 1; i < positions.Count; i++)
+    {
+      Vector2 prefPointPos = positions[i-1];
+      Vector2 pointPos = positions[i];
+      float tempDistance = Vector2.Distance(prefPointPos, pointPos);
+      float deltaDistance = targetDistance;
+      targetDistance -= tempDistance;
+
+      if(targetDistance <= 0)
+      {
+        float t = Mathf.InverseLerp(0, tempDistance, deltaDistance);
+        Vector2 dir = pointPos - prefPointPos;
+        Vector2 pos = prefPointPos + dir * t;
+        Vector3 currentPos = new Vector3(pos.x, 0f, pos.y);
+        return currentPos;
+      }
+    }
+
+    return Vector2.zero;
   }
 
   private void DestroyAllPlayer()
